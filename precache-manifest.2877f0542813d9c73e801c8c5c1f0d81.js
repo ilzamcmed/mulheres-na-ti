7941d5246f4c19b2fb7fbd92462cc36f24717281
Ilza <ilzammed2@gmail.com>
2020-07-14 16:49:54 -0300

    Updates

---
+++ precache-manifest.2877f0542813d9c73e801c8c5c1f0d81.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83cb0bba667a01c68dcf963938a1f5c0",
+    "url": "/mulheres-na-ti/index.html"
+  },
+  {
+    "revision": "cb4a72a7965958adbe58",
+    "url": "/mulheres-na-ti/static/css/main.fe813988.chunk.css"
+  },
+  {
+    "revision": "786042957522ff0a32e3",
+    "url": "/mulheres-na-ti/static/js/2.85c64d3f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/mulheres-na-ti/static/js/2.85c64d3f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb4a72a7965958adbe58",
+    "url": "/mulheres-na-ti/static/js/main.eb217149.chunk.js"
+  },
+  {
+    "revision": "4a12099ed9f3752b83d4",
+    "url": "/mulheres-na-ti/static/js/runtime-main.1cc63b6b.js"
+  }
+]);
