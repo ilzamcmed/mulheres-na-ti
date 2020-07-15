37b35124569e063512d1c814838251e7113f57a9
Ilza <ilzammed2@gmail.com>
2020-07-14 22:09:06 -0300

    Updates

---
+++ precache-manifest.1d0b858fb967e438bfe5348e2e9075f3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb6194d78c1cc8861d32748170eb962e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5f9f6e606ec6782b578b",
+    "url": "/static/css/main.fe813988.chunk.css"
+  },
+  {
+    "revision": "016de833eb36aa4176ae",
+    "url": "/static/js/2.410e3582.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.410e3582.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f9f6e606ec6782b578b",
+    "url": "/static/js/main.c70a9c11.chunk.js"
+  },
+  {
+    "revision": "ac33e9425f7b4f2ec933",
+    "url": "/static/js/runtime-main.6272486e.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/mulheres-na-ti/precache-manifest.5a2fd1bd79ab962c0bbfd47b5acba226.js"
+  "/precache-manifest.1d0b858fb967e438bfe5348e2e9075f3.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/mulheres-na-ti/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
